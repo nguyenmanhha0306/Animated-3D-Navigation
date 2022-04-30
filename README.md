8b002bbe148f1bbd3c40565c3aff54b631e21129
Ha <ha030600@gmail.com>
2022-04-30 23:02:28 +0700

    full code

---
+++ README.md
@@ -0,0 +1,2 @@
+![image](https://user-images.githubusercontent.com/65169088/166113068-1a5b9cf5-0802-48c8-9af6-8aaff17fd746.png)
+![image](https://user-images.githubusercontent.com/65169088/166113076-c0040a57-d65c-468f-a251-b7d8e96b567c.png)
--- src/components/layout/MainLayout.jsx
+++ src/components/layout/MainLayout.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import "./main-layout.scss";
+import { Outlet } from "react-router-dom";
+import Header from "../common/header/Header";
+import Menu from "../common/menu/Menu";
+import { useState } from "react";
 
 const MainLayout = () => {
-  return <div>MainLayout</div>;
+  const [active, setActive] = useState(false);
+
+  return (
+    <div className={`container ${active ? "active" : ""}`}>
+      <Header active={active} setActive={setActive} />
+      <div className="main-container">
+        <div className="main">
+          <div className="content">
+            <div className="overlay">
+              <Outlet />
+            </div>
+          </div>
+        </div>
+      </div>
+      <Menu active={active} setActive={setActive} />
+    </div>
+  );
 };
 
 export default MainLayout;
